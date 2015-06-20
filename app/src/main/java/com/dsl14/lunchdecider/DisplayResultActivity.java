2a1be42dbfa4cdea68955967e8424dabfcf0e8f0
Donut <b0290259@ntu.edu.tw>
2015-06-20 18:04:06 +0800

    Disable more fucking buttons

--- app/src/main/java/com/dsl14/lunchdecider/DisplayResultActivity.java
+++ app/src/main/java/com/dsl14/lunchdecider/DisplayResultActivity.java
@@ -41,11 +41,6 @@ public class DisplayResultActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
