db685893a44d7df60848006f56e8ac40eeca6737
Eug83 <Eug112358@gmail.com>
2015-06-21 19:12:42 +0800

    add map

--- app/src/main/java/com/dsl14/lunchdecider/MainActivity.java
+++ app/src/main/java/com/dsl14/lunchdecider/MainActivity.java
@@ -141,7 +141,7 @@ public class MainActivity extends ActionBarActivity
     public void decide(View view){
         viewNow = view;
         addRest = false;
-        intent = new Intent(this, DisplayResultActivity.class);
+        intent = new Intent(this, searchResultMapsActivity.class);
         Thread t = new thread();
         t.start();
     }
